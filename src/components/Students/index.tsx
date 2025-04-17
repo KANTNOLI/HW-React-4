79caa41270389060ffa2c36cca1b188d988cc6f7
KANTNOLI <zahargysev02@gmail.com>
2025-04-17 21:18:46 +0300

    3

--- src/components/Students/index.tsx
+++ src/components/Students/index.tsx
@@ -1,7 +1,35 @@
 import style from "./Students.module.css";
+import { StdIntf } from "../../Interfaces.interface";
+
+const rand = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const StudentsList = Array.from(
+  { length: 10 },
+  (): StdIntf => ({
+    name: `Name rand(${rand(0, 1000)})`,
+    money: rand(0, 1000),
+    group: rand(1000, 1000000),
+    mail: `mailrand(${rand(0, 1000)})@mail.com`,
+  })
+);
 
 function Students() {
-  return <section className={style.body}>asd</section>;
+  console.log(StudentsList);
+
+  return (
+    <section className={style.body}>
+      {StudentsList.map((Std, id) => (
+        <div key={id} className={style.cart}>
+          <p>{Std.name}</p>
+          <p>{Std.group}</p>
+          <p>{Std.money}</p>
+          <p>{Std.mail}</p>
+        </div>
+      ))}
+    </section>
+  );
 }
 
 export default Students;
